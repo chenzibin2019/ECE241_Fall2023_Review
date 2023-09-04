9176b205b83f01f5c6252c0065642d39c9079fc8
zibin <chenzibin2017@outlook.com>
2023-09-03 23:18:01 -0400

    basic

---
+++ control_statement.py
@@ -0,0 +1,9 @@
+x = int(input("Enter a number: "))
+
+if x < 0:
+    print("x is negative")
+elif x == 0:
+    print("x is zero")
+else:
+    print("x is positive")
+
---
+++ variable.py
@@ -0,0 +1,8 @@
+a = 5
+b = 7.0
+print(a + b)
+c = True
+s1 = 'Hello, world'
+s2 = "Another string"
+if c:
+    print(s1 + "\n" + s2)
