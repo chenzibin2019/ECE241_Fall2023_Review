7b33863b35671ca82ecf2080ef2737d2fa800b02
zibin <chenzibin2017@outlook.com>
2023-09-03 22:56:10 -0400

    Add operation.py

---
+++ operation.py
@@ -0,0 +1,26 @@
+
+
+# Add and subtraction
+print(3 + 4 - 2)
+
+# Multiplication
+# print(3 * 4)
+
+# Division
+# print(12 / 3)
+# print(10 / 3)
+
+# Exponentiation
+# print(2 ** 10)
+
+# Remainder
+# print(12 % 5)
+
+# Concatenation
+# print("I Love " + "Python")
+# print("I Love " + "Python" + 3)
+
+# Comparison
+# print(4 == 2)
+# print(1 + 1 == 2)
+
