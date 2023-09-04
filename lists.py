5e711d06f81a4755aa323c3288042d479d4848e0
zibin <chenzibin2017@outlook.com>
2023-09-03 23:38:51 -0400

    lists.py

--- lists.py
+++ lists.py
@@ -1,31 +1,44 @@
 
 # Create a list
 alist = [10, 4, 66, 3, 100]
-print(alist)
+print(alist, len(alist))
 
 # Append an element (at the end)
-alist.append(9)
-print(alist)
+# alist.append(9)
+# print(alist)
 
 # insert an element at specific position
-alist.insert(2, 66)
-print(alist)
+# alist.insert(2, 66)
+# print(alist)
 
 # Pop an element
-item = alist.pop()
-print(item, alist)
+# item = alist.pop()
+# print(item, alist)
 
 # Sort the list (in place)
-alist.sort()
-print(alist)
+# alist.sort()
+# print(alist)
 # make it descending...
-alist.reverse()
-print(alist)
+# alist.reverse()
+# print(alist)
 
 # Returns the index of first occurrence
-index_of_10 = alist.index(10)
-print(index_of_10)
+# index_of_10 = alist.index(10)
+# print(index_of_10)
 
 # Remove the first occurrence of an element
-alist.remove(66)
-print(alist)
+# alist.remove(66)
+# print(alist)
+
+# Slicing, 1 -> 4 (last item not included!)
+# print(alist[1:5])
+
+# List comprehensions
+# squares = []
+# for i in range(5):
+#     squares.append(i ** 2)
+# print(squares)
+#
+# # is equivalent of saying...
+# squares2 = [i ** 2 for i in range(5)]
+# print(squares2)
---
+++ string-as-a-list.py
@@ -0,0 +1,24 @@
+# Create a string/list of characters
+s = 'Hello'  # Equivalent of saying s = "Hello"
+
+# Iterate through its elements
+# for c in s:
+#     print(c)
+
+# Create another string
+# r = ' world'
+# Concatenate strings
+# print(s + r)
+
+# String repetition
+# print(s * 3)
+
+# Since it is a list...
+# Return element at position 2 (index 1)
+# print(s[1])
+
+# Get the length of a string
+# print(len(s))
+
+# slicing
+# print(s[1:4])
